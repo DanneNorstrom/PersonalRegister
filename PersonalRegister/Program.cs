53779e53590a0e05f46bd2e9b5862eaeea275bf5
dannor0608 <93254330+dannor0608@users.noreply.github.com>
2024-09-10 14:34:26 +0200

    Add project files.

---
+++ PersonalRegister/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace PersonalRegister
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int MaxAnstallda = 10;
+            string[] namn = new string[MaxAnstallda];
+            int[] lon = new int[10];
+
+            Console.WriteLine("Ange antal anställda att mata in: (Max 10)");
+            int antalAnstallda = Convert.ToInt32(Console.ReadLine());
+
+            for (int c = 0; c < antalAnstallda; c++)
+            {
+                Console.WriteLine("Ange namn:");
+                namn[c] = Console.ReadLine();
+
+                Console.WriteLine("Ange lön:");
+                lon[c] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("Tryck på valfri tangent på tangentbordet för att skriva ut registret");
+            Console.ReadKey();
+
+            for (int c = 0; c < antalAnstallda; c++)
+            {
+                Console.WriteLine(namn[c] +" " + "Lön = " + lon[c] +"kr");
+            }
+
+            Console.ReadKey();  
+        }
+    }
+}
